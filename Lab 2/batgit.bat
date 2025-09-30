9792f14bed45a3860d159792aef104b8c5915cf8
Dasha <dashutkaab@gmail.com>
2025-09-30 19:47:25 +0300

    Update batgit.bat
    добавлена отвязка от git

--- Lab 2/batgit.bat
+++ Lab 2/batgit.bat
@@ -1,14 +1,20 @@
 @ECHO OFF
 ECHO Setting up Git configuration
 
-REM установка имени пользователЯ
+REM ГіГ±ГІГ Г­Г®ГўГЄГ  ГЁГ¬ГҐГ­ГЁ ГЇГ®Г«ГјГ§Г®ГўГ ГІГҐГ«Гџ
 git config --global user.name "%1"
 
-REM установка email
+REM ГіГ±ГІГ Г­Г®ГўГЄГ  email
 git config --global user.email "%2"
 
 ECHO Git configuration completed!
 ECHO User name: "%1"
 ECHO User email: "%2"
-
+ECHO Press any button to remove git configuration
 PAUSE
+git config --global --unset user.name
+git config --global --unset user.email
+
+ECHO Git configuration removed!
+PAUSE
+
